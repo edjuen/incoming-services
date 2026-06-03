d64cb3a163205ed900d71708fb7132783d46104b
edjuen <edjuen@gmail.com>
2026-06-03 00:47:43 -0600

    Permitir acceso al panel Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
 
     public function canAccessPanel(Panel $panel): bool
     {
-    	return str_ends_with($this->email, '@example.com');
+    	return true;
     }
 
     /**
